98d7d15407d6de545d8d8d331cc1e59ee67ba544
Bruk Teshome <brukteshome21@gmail.com>
2024-07-24 19:19:34 +0300

    feat: add pool interface

--- app/utils/Liquidity.tsx
+++ app/utils/Liquidity.tsx
@@ -10,9 +10,30 @@ import { getProvider, getSigner, setUpWeb3 } from '../web3';
 // Import your Liquidity contract ABI and address
 import { liquidityContractAddress, liquiditydAbi } from '../constants';
 
+interface PoolInfo {
+  address: string;
+  token0: string;
+  token1: string;
+  fee: number;
+}
 
 const LiquidityMarket: React.FC = () => {
+  const [tokenA, setTokenA] = useState<string>('');
+  const [tokenB, setTokenB] = useState<string>('');
+  const [fee, setFee] = useState<FeeAmount>(FeeAmount.MEDIUM);
+  const [poolAddress, setPoolAddress] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+  const [pools, setPools] = useState<PoolInfo[]>([]);
+  const [selectedPool, setSelectedPool] = useState<string>('');
+  const [liquidityAmount, setLiquidityAmount] = useState<string>('');
+  const [approveAmount, setApproveAmount] = useState<string>('');
 
+  useEffect(() => {
+    setUpWeb3();
+  }, []);
+
+  
+  
 
   return (
     <div>
