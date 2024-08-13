ae6ba006892f2bc79165e076bcbfbb86159c588a
Bruk Teshome <brukteshome21@gmail.com>
2024-08-13 11:32:37 +0300

    style: add to home page

--- app/utils/Liquidity.tsx
+++ app/utils/Liquidity.tsx
@@ -1,10 +1,11 @@
 // src/components/LiquidityMarket.tsx
 "use client";
 import React, { useState, useEffect } from 'react';
-import Modal from './Modal'; // Import the Modal component
-import CheckPool from './CheckPool'; // Import the Check Pool component
-import AddLiquidity from './AddLiquidity'; // Import the Add Liquidity component
-import { fetchPools } from '../utils/uniswapClient'; // Import the fetchPools function
+import Modal from './Modal'; 
+import CheckPool from './CheckPool'; 
+import AddLiquidity from './AddLiquidity'; 
+import { fetchPools } from '../utils/uniswapClient';
+import Image from 'next/image';
 
 interface PoolInfo {
   address: string;
@@ -32,22 +33,26 @@ const LiquidityMarket: React.FC = () => {
 
   return (
     <div>
-      <div className="relative h-[89vh]">
+      <div className="relative h-[100vh]">
         <div
           className="absolute inset-0 bg-[url('/cryp.jpg')] bg-cover bg-center opacity-95"
         ></div>
-        <div className="relative flex flex-col justify-center items-center h-full px-4 sm:px-6 lg:px-8">
-          <h3 className="text-2xl font-normal font-serif mt-6 text-white">Coin Market</h3>
+        <div className="relative flex flex-col justify-center pt-8 items-center h-full px-4 sm:px-6 lg:px-8">
+          <h3 className="text-2xl font-normal font-serif mt-6 text-orange-400">Coin Market</h3>
           <h1 className="text-6xl font-medium text-orange-400 font-serif text-white my-10">Create Liquidity Market Place</h1>
           <p className="text-white w-full max-w-3xl text-center">
             Looking to capitalize on market opportunities and increase the liquidity
             of your assets? Our platform provides the perfect environment for
             investment and market access.
           </p>
-          <div className='flex mt-8 gap-5'>
-            <button className='text-orange-400 text-[20px] border-[2px] border-orange-400 px-4 py-2 rounded-lg' onClick={() => setIsPoolModalOpen(true)}>
+          <div>
+            
+          </div>
+          <div className='flex items-center mt-8 gap-5'>
+            <button className='text-orange-400 text-[20px] max-h-[60px] border-[2px] border-orange-400 px-4 py-2 rounded-lg' onClick={() => setIsPoolModalOpen(true)}>
               Add Pool
             </button>
+            <Image className='mt-10' src="/buyS.png" alt="crypto" width={320} height={120} />
             <button className='text-orange-400 text-[20px] border-[2px] border-orange-400 px-4 py-2 rounded-lg' onClick={() => setIsLiquidityModalOpen(true)}>
               Add Liquidity
             </button>
